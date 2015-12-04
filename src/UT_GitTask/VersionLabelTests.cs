03d436783494ce4451debe5814101ff68ae377b2
Antonio Aversa <aaversa@gsx.com>
2015-12-04 09:09:29 +0100

    Fix VersionLabel.Parse tests

--- src/UT_GitTask/VersionLabelTests.cs
+++ src/UT_GitTask/VersionLabelTests.cs
@@ -30,43 +30,43 @@ namespace UT_GitTask
         public void TestVersionTag_Parse()
         {
             // Basic valid
-            Assert.AreEqual(new VersionLabel("1", 1), VersionLabel.Parse("1"));
-            Assert.AreEqual(new VersionLabel("1.0", 1, 0), VersionLabel.Parse("1.0"));
-            Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0"));
-            Assert.AreEqual(new VersionLabel("1.0.0.0", 1, 0, 0, 0), VersionLabel.Parse("1.0.0.0"));
+            Assert.AreEqual(new VersionLabel("1", 1), VersionLabel.Parse("1", string.Empty));
+            Assert.AreEqual(new VersionLabel("1.0", 1, 0), VersionLabel.Parse("1.0", string.Empty));
+            Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0", string.Empty));
+            Assert.AreEqual(new VersionLabel("1.0.0.0", 1, 0, 0, 0), VersionLabel.Parse("1.0.0.0", string.Empty));
 
             // Basic invalid
-            Assert.IsNull(VersionLabel.Parse("-1"));
-            Assert.IsNull(VersionLabel.Parse("1.."));
-            Assert.IsNull(VersionLabel.Parse("1.-1"));
-            Assert.IsNull(VersionLabel.Parse("1.a"));
-            Assert.IsNull(VersionLabel.Parse("1. "));
-            Assert.IsNull(VersionLabel.Parse("1.1.a"));
-            Assert.IsNull(VersionLabel.Parse("1.1.1.a"));
-            Assert.IsNull(VersionLabel.Parse("1.1.1.-1"));
+            Assert.IsNull(VersionLabel.Parse("-1", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1..", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1.-1", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1.a", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1. ", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1.1.a", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1.1.1.a", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("1.1.1.-1", string.Empty));
 
             // With suffix
-            Assert.AreEqual(new VersionLabel("1 ", 1), VersionLabel.Parse("1 "));
-            Assert.AreEqual(new VersionLabel("1_ci", 1), VersionLabel.Parse("1_ci"));
-            Assert.AreEqual(new VersionLabel("1.0_ci", 1, 0), VersionLabel.Parse("1.0_ci"));
+            Assert.AreEqual(new VersionLabel("1 ", 1), VersionLabel.Parse("1 ", string.Empty));
+            Assert.AreEqual(new VersionLabel("1_ci", 1), VersionLabel.Parse("1_ci", string.Empty));
+            Assert.AreEqual(new VersionLabel("1.0_ci", 1, 0), VersionLabel.Parse("1.0_ci", string.Empty));
         }
 
         [TestMethod]
         public void TestVersionTag_ParseWithPrefix()
         {
             // Basic valid
-            Assert.AreEqual(new VersionLabel("v1", 1), VersionLabel.Parse("v1"));
-            Assert.AreEqual(new VersionLabel("vv1.0", 1, 0), VersionLabel.Parse("vv1.0"));
-            Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0"));
-            Assert.AreEqual(new VersionLabel("v1.0.0.1", 1, 0, 0, 1), VersionLabel.Parse("v1.0.0.1"));
+            Assert.AreEqual(new VersionLabel("v1", 1), VersionLabel.Parse("v1", string.Empty));
+            Assert.AreEqual(new VersionLabel("vv1.0", 1, 0), VersionLabel.Parse("vv1.0", string.Empty));
+            Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0", string.Empty));
+            Assert.AreEqual(new VersionLabel("v1.0.0.1", 1, 0, 0, 1), VersionLabel.Parse("v1.0.0.1", string.Empty));
 
             Assert.AreEqual(new VersionLabel("v1", 1), VersionLabel.Parse("v1", "v"));
             Assert.AreEqual(new VersionLabel("vv1.0", 1, 0), VersionLabel.Parse("vv1.0", "vv"));
             Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0", ""));
             Assert.AreEqual(new VersionLabel("v1.0.0.1", 1, 0, 0, 1), VersionLabel.Parse("v1.0.0.1", "v"));
 
-            Assert.IsNull(VersionLabel.Parse("_1.0.0.0"));
-            Assert.IsNull(VersionLabel.Parse(".1.0.0.0"));
+            Assert.IsNull(VersionLabel.Parse("_1.0.0.0", string.Empty));
+            Assert.IsNull(VersionLabel.Parse(".1.0.0.0", string.Empty));
             Assert.IsNull(VersionLabel.Parse("v1", "aa"));
             Assert.IsNull(VersionLabel.Parse("vv1.0", "v"));
             Assert.IsNull(VersionLabel.Parse("1.0.0", "v"));
@@ -76,12 +76,12 @@ namespace UT_GitTask
         public void TestVersionTag_ParseWithSuffix()
         {
             // Basic valid
-            Assert.AreEqual(new VersionLabel("v1", 1), VersionLabel.Parse("v1"));
-            Assert.AreEqual(new VersionLabel("vv1.0", 1, 0), VersionLabel.Parse("vv1.0"));
-            Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0"));
-            Assert.AreEqual(new VersionLabel("v1.0.0.1", 1, 0, 0, 1), VersionLabel.Parse("v1.0.0.1"));
-            Assert.IsNull(VersionLabel.Parse("_1.0.0.0"));
-            Assert.IsNull(VersionLabel.Parse(".1.0.0.0"));
+            Assert.AreEqual(new VersionLabel("v1", 1), VersionLabel.Parse("v1", string.Empty));
+            Assert.AreEqual(new VersionLabel("vv1.0", 1, 0), VersionLabel.Parse("vv1.0", string.Empty));
+            Assert.AreEqual(new VersionLabel("1.0.0", 1, 0, 0), VersionLabel.Parse("1.0.0", string.Empty));
+            Assert.AreEqual(new VersionLabel("v1.0.0.1", 1, 0, 0, 1), VersionLabel.Parse("v1.0.0.1", string.Empty));
+            Assert.IsNull(VersionLabel.Parse("_1.0.0.0", string.Empty));
+            Assert.IsNull(VersionLabel.Parse(".1.0.0.0", string.Empty));
         }
 
         [TestMethod]
